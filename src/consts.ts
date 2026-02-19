47c0ca0b3238acaaa180607e5c716b09c0c8aa19
Nobuo Kato <katonobu4649@gmail.com>
2026-02-19 13:14:48 +0000

    ページタイトル変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '天気予報解説資料/各種天気図/衛星写真 | katonobu';
+export const SITE_DESCRIPTION = '気象予報士試験午後の対策用に気象庁発表の短期/週間天気予報解説資料、対応時刻の各種天気図/衛星写真を見やすくまとめました。1日3回更新。';
